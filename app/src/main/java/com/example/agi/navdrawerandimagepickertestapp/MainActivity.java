fa95fcecf77c5fff720fe51a225dc698d35406e0
Agi Karasugi <karasugiputuagi@outlook.com>
2019-03-27 00:18:04 +0700

    updates and refactoring (1)

--- app/src/main/java/com/example/agi/navdrawerandimagepickertestapp/MainActivity.java
+++ app/src/main/java/com/example/agi/navdrawerandimagepickertestapp/MainActivity.java
@@ -1,23 +1,14 @@
 package com.example.agi.navdrawerandimagepickertestapp;
 
-import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
-import android.provider.MediaStore;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.example.agi.navdrawerandimagepickertestapp.fragment.FragmentAbout;
 import com.example.agi.navdrawerandimagepickertestapp.fragment.FragmentHistory;
@@ -25,7 +16,7 @@ import com.example.agi.navdrawerandimagepickertestapp.fragment.FragmentHome;
 import com.example.agi.navdrawerandimagepickertestapp.fragment.FragmentSettings;
 import com.example.agi.navdrawerandimagepickertestapp.fragment.FragmentTextEditor;
 
-import java.io.IOException;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,7 +30,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         final ActionBar actionBar = getSupportActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        Objects.requireNonNull(actionBar).setDisplayHomeAsUpEnabled(true);
         actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
         drawerLayout = findViewById(R.id.drawer_layout);
@@ -55,35 +46,35 @@ public class MainActivity extends AppCompatActivity {
                         menuItem.setChecked(true);
                         drawerLayout.closeDrawers();
 
-                        Toast toast;
+//                        Toast toast;
                         switch (menuItem.getItemId()){
                             case R.id.nav_home:
-                                toast = Toast.makeText(MainActivity.this, "Home selected", Toast.LENGTH_SHORT);
-                                toast.show();
+//                                toast = Toast.makeText(MainActivity.this, "Home selected", Toast.LENGTH_SHORT);
+//                                toast.show();
                                 actionBar.setTitle("SCRUM");
                                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentHome()).commit();
                                 return true;
                             case R.id.nav_history:
-                                toast = Toast.makeText(MainActivity.this, "History selected", Toast.LENGTH_SHORT);
-                                toast.show();
+//                                toast = Toast.makeText(MainActivity.this, "History selected", Toast.LENGTH_SHORT);
+//                                toast.show();
                                 actionBar.setTitle("HISTORY");
                                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentHistory()).commit();
                                 break;
                             case R.id.nav_textEditor:
-                                toast = Toast.makeText(MainActivity.this, "Text Editor selected", Toast.LENGTH_SHORT);
-                                toast.show();
+//                                toast = Toast.makeText(MainActivity.this, "Text Editor selected", Toast.LENGTH_SHORT);
+//                                toast.show();
                                 actionBar.setTitle("TEXT EDITOR");
                                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentTextEditor()).commit();
                                 break;
                             case R.id.nav_settings:
-                                toast = Toast.makeText(MainActivity.this, "Settings selected", Toast.LENGTH_SHORT);
-                                toast.show();
+//                                toast = Toast.makeText(MainActivity.this, "Settings selected", Toast.LENGTH_SHORT);
+//                                toast.show();
                                 actionBar.setTitle("SETTINGS");
                                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentSettings()).commit();
                                 break;
                             case R.id.nav_about:
-                                toast = Toast.makeText(MainActivity.this, "About selected", Toast.LENGTH_SHORT);
-                                toast.show();
+//                                toast = Toast.makeText(MainActivity.this, "About selected", Toast.LENGTH_SHORT);
+//                                toast.show();
                                 actionBar.setTitle("ABOUT");
                                 getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new FragmentAbout()).commit();
                                 break;
